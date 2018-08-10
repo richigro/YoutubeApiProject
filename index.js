b2d83f3e6ca3f9b43c0a93d5be89f2f8ecb64d4a
Ricardo Guerrero <richi_gro@hotmail.com>
2018-08-10 11:35:31 -0500

    deleted grid.css added new style rules for css, added accesibility roles for aria, and changed the views

--- index.js
+++ index.js
@@ -11,11 +11,15 @@ function addToPage(video) {
 
 function renderVideoThumbnail(data) {
   return `
-      <div id="${data.id.videoId}" class="js-thumbnail    thumbnail">
-        <img class="js-img" src="${data.snippet.thumbnails.medium.url}" alt="${data.snippet.title}">
-        <h1>${data.snippet.title}</h1>
-        <p>${data.snippet.channelTitle}</p>
-        <p>${data.snippet.publishedAt}</p>
+      <div id="${data.id.videoId}" class="js-thumbnail thumbnail">
+        <div id="${data.id.videoId}" class="thumbnail-img-container">
+          <img class="js-img thumbnail-img" src="${data.snippet.thumbnails.medium.url}" alt="" />
+        </div>
+        <div class="thumbnail-info">
+          <h1>${data.snippet.title}</h1>
+          <p>${data.snippet.channelTitle} 2.3B views ${data.snippet.publishedAt}</p>
+          <p>${data.snippet.description}</p>
+        </div>  
       </div>
   `;
 }
@@ -32,7 +36,7 @@ function getDataFromApi(searchTerm, callback) {
 function printResult(data) {
   // array containing data items
   const objectArray = data.items;
-  // console.log(objectArray[0].id.videoId);
+  //  console.log(objectArray[0]);
   
   objectArray.forEach((videoObject) => {
      addToPage(renderVideoThumbnail(videoObject));
@@ -60,7 +64,7 @@ function search(){
 function viewingPage(id) {
   return `
     <div class="video-view" >
-        <iframe class="video" width="560" height="315" src="https://www.youtube.com/embed/${id}" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+        <iframe class="video" width="560" height="315" src="https://www.youtube.com/embed/${id}?autoplay=1" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
       </div> 
   
   `;
@@ -75,6 +79,8 @@ function goToVideo() {
     // open another page with tagret url
     // remove video on page
     let videoId = event.target.parentElement.getAttribute("id");
+    console.log("===========================================");
+    console.log(videoId);
     removeLastView();
      // render new view
     addToPage(viewingPage(videoId));
@@ -87,4 +93,4 @@ function runApp() {
   goToVideo();
 }
 
-$(runApp);
+ $(runApp);
